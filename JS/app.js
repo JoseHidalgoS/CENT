acdd5e00352fd4727a6d46238e0290864bf7a250
Jose <hidalgojoseenrique58@gmail.com>
2023-08-19 12:51:14 -0400

    pe

---
+++ JS/app.js
@@ -0,0 +1,24 @@
+let barras = [document.getElementById("hr1"),document.getElementById("hr2"),document.getElementById("hr2")];
+
+let girar = true
+let menu = document.getElementById("nav")
+
+for(let i = 0; i < barras.length; i++){
+    barras[i].addEventListener("click", ()=>{
+        menu.style.transition = "0.5s"
+        barras[i].style.transition = "0.5s"
+        barras[i].style.position = "relative"
+        if (girar){
+            barras[i].style.transition = "0.5s"
+            barras[1].style.opacity = "0"
+            barras[0].style.transform = "rotate(45deg)"
+            barras[0].style.top = "12px"
+            barras[2].style.transform = "rotate(-45deg)"
+            barras[2].style.bottom= "15px"
+            barras[2].style.left = "-1px"
+            girar = false
+            menu.style.zIndex = 2000
+            menu.style.opacity = 1
+        }
+    })
+}
